13b4db2be4eae35815f22f8cafe90fb10f139a76
cajaun <cajaun.campbell@gmail.com>
2026-02-25 10:28:24 -0500

    Pushing new template

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import {
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import { Stack } from "expo-router";
+import "./global.css";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+
+        <Stack>
+        </Stack>
+
+    </GestureHandlerRootView>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { router } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+     className = "gap-y-6"
+    >
+
+    </View>
+  );
+}
